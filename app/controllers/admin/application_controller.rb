ab6c7e3eb3ff04b4e66c8dabc56dc5742525373f
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-07-28 09:21:17 +0700

    fix: rename method to match devise naming convention

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,11 +2,11 @@ module Admin
   class ApplicationController < ::ApplicationController
     layout 'admin'
 
-    before_action :require_admin!
+    before_action :authenticate_admin!
 
     private
 
-    def require_admin!
+    def authenticate_admin!
       return if current_user.admin?
 
       redirect_to dashboard_path, alert: 'You are not authorized to access this page.'
