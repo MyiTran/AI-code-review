43c8d2c25fb0e746f24b3bdf1e74a83125732cbc
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-12 10:05:57 +0700

    feat: setting UI

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,21 +1,5 @@
 class SettingsController < ApplicationController
-  skip_before_action :authenticate_user!
-  SETTINGS_TABS = %w[
-    profile
-    ai_review
-    notifications
-    github
-    billing
-  ].freeze
-
   def show
-    @settings = Mock::Settings.data
-
-    @active_tab =
-      if SETTINGS_TABS.include?(params[:tab])
-        params[:tab]
-      else
-        'profile'
-      end
+    @user = current_user
   end
 end
