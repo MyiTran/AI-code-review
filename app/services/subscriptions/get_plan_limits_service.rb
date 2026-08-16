4bf7e30e2c4b323be8a95ebd50c6774ddb52fcd6
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-16 17:42:18 +0700

    fix: add free, pro plan

--- app/services/subscriptions/get_plan_limits_service.rb
+++ app/services/subscriptions/get_plan_limits_service.rb
@@ -1,6 +1,6 @@
 module Subscriptions
   class GetPlanLimitsService < ApplicationService
-    FREE = { repositories: 8, pull_requests: 22, reviews: 38 }.freeze
+    FREE = { repositories: 3, pull_requests: 30, reviews: 60 }.freeze
     PRO = { repositories: 50, pull_requests: 500, reviews: 1_000 }.freeze
 
     def initialize(user)
