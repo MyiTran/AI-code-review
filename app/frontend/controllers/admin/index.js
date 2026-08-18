539945ddbbd784c7e22542dca8ea110c4ff5a76e
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-18 15:01:57 +0700

    fix: rubocop style

--- app/frontend/controllers/admin/index.js
+++ app/frontend/controllers/admin/index.js
@@ -1,5 +1,5 @@
 import { setupStimulus } from '@/utils/setupStimulus';
-import { togglePassword } from './password_toggle'
+import { togglePassword } from './password_toggle';
 
-window.togglePassword = togglePassword
+window.togglePassword = togglePassword;
 setupStimulus(import.meta.glob('./**/*_controller.js', { eager: true }));
