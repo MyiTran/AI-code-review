69beb0b9e0772d208e5ba0efc12c6f2366e735a4
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-18 10:34:35 +0700

    feat: form filter users

--- app/policies/admin/user_policy.rb
+++ app/policies/admin/user_policy.rb
@@ -1,4 +1,9 @@
 module Admin
   class UserPolicy < BasePolicy
+    class Scope < BasePolicy::Scope
+      def resolve
+        super.where.not(id: Role.where(name: %w[admin super_admin]).joins(:users).select('users.id'))
+      end
+    end
   end
 end
