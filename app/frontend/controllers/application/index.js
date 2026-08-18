47ab22e62c88dc42a4764234e515870c0ca8742c
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-18 15:08:27 +0700

    fix: node version

--- app/frontend/controllers/application/index.js
+++ app/frontend/controllers/application/index.js
@@ -1,5 +1,5 @@
 import { setupStimulus } from '@/utils/setupStimulus';
 import './ai_review_toggle';
 import './review_tabs';
-import '../admin'
+import '../admin';
 setupStimulus(import.meta.glob('./**/*_controller.js', { eager: true }));
