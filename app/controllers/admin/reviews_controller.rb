5370225f6fd4d956dc6672969862c296a5377d90
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-19 17:17:26 +0700

    fix: service name

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -5,7 +5,7 @@ module Admin
       @repository = @user.repositories.find(params.expect(:repository_id))
       @pull_request = @repository.pull_requests.find(params.expect(:pull_request_id))
       @review = @pull_request.reviews.includes(:ai_model).find(params.expect(:id))
-      @changed_files = Github::FetchPullRequestDiff.call(@pull_request, base_sha: @review.base_commit_sha, head_sha: @review.commit_sha)
+      @changed_files = Github::FetchPullRequestDiffService.call(@pull_request, base_sha: @review.base_commit_sha, head_sha: @review.commit_sha)
     end
   end
 end
