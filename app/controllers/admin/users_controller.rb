44fdb708fa1c5e55a32721bea926d94f76a1deaf
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-19 17:15:17 +0700

    fix: scope name

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,7 +13,7 @@ module Admin
       @user = User.find(params.expect(:id))
       authorize @user, policy_class: Admin::UserPolicy
 
-      repositories = @user.repositories.includes(:ai_model).search_by_keyword(params[:q]).order(created_at: :desc)
+      repositories = @user.repositories.includes(:ai_model).by_keyword(params[:q]).order(created_at: :desc)
       @pagy, @repositories = pagy(repositories, limit: 10)
       @pull_request_counts = PullRequest.where(repository_id: @repositories.ids).group(:repository_id).count
     end
