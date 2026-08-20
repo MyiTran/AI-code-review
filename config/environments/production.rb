164cb600450cc70077ff019f120e36159206016f
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-21 00:16:12 +0700

    fix: Configure production deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Store files locally.
-  config.active_storage.service = :amazon
+  config.active_storage.service = ENV.fetch('ACTIVE_STORAGE_SERVICE', 'amazon').to_sym
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
