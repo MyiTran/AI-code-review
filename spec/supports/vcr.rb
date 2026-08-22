a59bc41427227f0813e7245dc4c1c907dab7f3df
MyiTran <myitrantranthitramyttii26092005@gmail.com>
2026-08-22 14:20:32 +0700

    Fix: GitHub installation VCR test-v2

--- spec/supports/vcr.rb
+++ spec/supports/vcr.rb
@@ -8,6 +8,7 @@ VCR.configure do |config|
   config.ignore_host 'chromedriver.storage.googleapis.com'
   config.cassette_library_dir = File.expand_path('../cassettes', __dir__)
   config.configure_rspec_metadata!
+
   config.default_cassette_options = {
     record: ENV['CI'] ? :none : :once,
     record_on_error: false,
@@ -20,9 +21,15 @@ VCR.configure do |config|
     end
   end
 
-  config.filter_sensitive_data('[GITHUB_APP_ID]') { ENV['GITHUB_APP_ID'] }
-  config.filter_sensitive_data('[GITHUB_APP_PRIVATE_KEY]') { ENV['GITHUB_APP_PRIVATE_KEY'] }
-  config.filter_sensitive_data('[GITHUB_WEBHOOK_SECRET]') { ENV['GITHUB_WEBHOOK_SECRET'] }
-  config.filter_sensitive_data('[GEMINI_API_KEY]') { ENV['GEMINI_API_KEY'] }
-  config.filter_sensitive_data('[TEST_GITHUB_INSTALLATION_ID]') { ENV['TEST_GITHUB_INSTALLATION_ID'] }
+  config.filter_sensitive_data('[GITHUB_APP_PRIVATE_KEY]') do
+    ENV['GITHUB_APP_PRIVATE_KEY']
+  end
+
+  config.filter_sensitive_data('[GITHUB_WEBHOOK_SECRET]') do
+    ENV['GITHUB_WEBHOOK_SECRET']
+  end
+
+  config.filter_sensitive_data('[GEMINI_API_KEY]') do
+    ENV['GEMINI_API_KEY']
+  end
 end
